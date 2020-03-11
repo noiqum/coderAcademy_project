57f612ef24adf701728edeb83a00dee686f4d688
unknown <onurcoskun99@gmail.com>
2020-03-11 13:39:07 +0300

    comment section finished

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 window.onscroll=function () {wScroll()};
 
 function wScroll() {
-    if(document.body.scrollTop> 80 || document.documentElement.scrollTop> 80){
+    if(document.body.scrollTop> 100 || document.documentElement.scrollTop> 100){
         document.getElementById("nav-bar").style.top="0"
     }else{
         document.getElementById("nav-bar").style.top="-100px"
