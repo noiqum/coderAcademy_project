6841ffde72020ec2db3b8465c087b1b6f3277193
unknown <onurcoskun99@gmail.com>
2020-03-14 20:50:08 +0300

    brochure request form finished

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
 
+window.onload=function () {
+    
+    var span=document.getElementById("sp");
+    var text=document.getElementById('name');
+    var email=document.getElementById("email");
+    var span1=document.getElementById("ea");
 
+    text.addEventListener("focus",move);
+    email.addEventListener('focus',move1);
+    
+
+    function move() {
+        span.classList.add("new");
+
+};
+    function move1() {
+        span1.classList.add("new");
+    };
+
+    
+
+};
 window.onscroll=function () {wScroll()};
 
 function wScroll() {
