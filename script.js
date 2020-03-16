5f13840fbfd14c041852b79ef97c7a942e839568
unknown <onurcoskun99@gmail.com>
2020-03-16 13:15:50 +0300

    frequently asked questions section update: 1. accordion part that was done with css target property was cancelled 2. add faq section javascript functionality for better solution

--- script.js
+++ script.js
@@ -13,15 +13,47 @@ window.onload=function () {
     function move() {
         span.classList.add("new");
 
-};
+    };
     function move1() {
         span1.classList.add("new");
     };
 
+    //frequently asked question section collapse function
+    
+
+    var questions =document.getElementsByClassName("question__button");
+    var i;
+   function trye() {
+       for(i=0;i<questions.length;i++){
+           console.log(questions[i]);
+           questions[i].addEventListener("click",funky);
+       };
+   };
+   trye();
+   function funky(btn) {
+       var content=btn.target.nextElementSibling;
+       var contentClass=content.getAttributeNode("class").value;
+       if(contentClass==="content"){
+            content.classList.remove("content");
+            content.classList.add("content-new");
+       }else{
+           content.classList.remove("content-new");
+           content.classList.add("content");
+       }
+   };
+
+    
+    
+        
+    
+    
     
 
 };
+
 window.onscroll=function () {wScroll()};
+
+
 
 function wScroll() {
     if(document.body.scrollTop> 100 || document.documentElement.scrollTop> 100){
